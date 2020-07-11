f2d0008db4a96e6aaf20c5136e9811ca5ebdfb29
jnosek <jnosek@gmail.com>
2020-07-10 20:24:10 -0400

    implement memorybus interface for abstraction

---
+++ AGC/Memory/IMemoryBus.cs
@@ -0,0 +1,16 @@
+﻿namespace Apollo.Virtual.AGC.Memory
+{
+    public interface IMemoryBus
+    {
+        OnesCompliment this[ushort a] { get; set; }
+        int MaxAddress { get; }
+
+        /// <summary>
+        /// The AGC has memory mapped registerss, so they need a method to regiser themselves to the MemoryBus
+        /// </summary>
+        /// <typeparam name="RegisterType"></typeparam>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        RegisterType MapRegister<RegisterType>(ushort address) where RegisterType : MemoryWord;
+    }
+}
